2def2f2816d076f4595311febe4916bfeb02a18b
menal ibrahim <menalibrahim67@gmail.com>
2023-04-01 12:13:41 +0300

    second commit

--- 2-express/2-express-basics.js
+++ 2-express/2-express-basics.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 
-app.get('/', (req, res) =>{
-    res.status(200).send('Home Page')
-})
+//setup static and middleware
+app.use(express.static('./public'))
 
-app.get('/about', (req, res) =>{
-    res.status(200).send('About Page')
-})
+//app.get('/', (req, res) =>{
+//    res.status(200).send('Home Page')
+//})
+//app.get('/', (req, res) =>{
+  //  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+//})
+
+//app.get('/about', (req, res) =>{
+   // res.status(200).send('About Page')
+//})
 
 app.all('*', (req, res) =>{
     res.status(404).send('<h1>Resource not found</h1>')
